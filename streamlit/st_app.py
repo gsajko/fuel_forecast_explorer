8814fa66038991da43c58a334097344b7fdfea92
gsajko <grzegorz.sajko@protonmail.com>
2021-08-16 20:07:44 +0200

    cleanup

--- streamlit/st_app.py
+++ streamlit/st_app.py
@@ -34,15 +34,17 @@ if "df_pivot" not in st.session_state:
 df_pivot = st.session_state.df_pivot
 # %%
 # sidebar
-start_date = st.sidebar.date_input("From: ", datetime.date(2019, 1, 1))
+start_date = st.sidebar.date_input("From: ", datetime.date(2021, 1, 1))
 end_date = st.sidebar.date_input("From: ", datetime.date(2021, 5, 1))
 fuel_type = st.sidebar.selectbox("fuel type", df_pivot.columns)
 resample_type = st.sidebar.selectbox(
     "resample period ", ["1H", "6H", "1D", "3D", "W"]
 )
 postcodes = st.sidebar.multiselect(
-    "Postcode: ", df.site_post_code.unique(), ["4417"]
+    "Postcode: ", df.site_post_code.unique(), ["4020"]
 )
+#TODO not all postcodes are displayed? no 4000
+
 
 dfx = df[
     (df.site_post_code.isin(postcodes)) & (df.fuel_type == fuel_type)
@@ -84,18 +86,29 @@ df_plot = (
     .mean()
     .bfill()
 )
-# st.write(df_plot.head())
+
+
+QLD_mean_plot = st.sidebar.checkbox("show QLD mean on the plot", value=True)
+postcode_mean_plot = st.sidebar.checkbox("show postcode mean on the plot")
 
 fig2 = go.Figure(
-    [
+)
+if QLD_mean_plot:
+    fig2.add_trace(
         go.Scatter(
             x=df_plot.index,
             y=df_plot[fuel_type],
             name="QLD mean",
             line=dict(color='rgb(210,210,210, 0.2)', width=6,),
-        )
-    ]
-)
+        ))
+# if postcode_mean_plot:
+#     fig2.add_trace(
+#         go.Scatter(
+#             x=df_plot.index,
+#             y=df_plot[fuel_type],
+#             name="QLD mean",
+#             line=dict(color='rgb(210,210,210, 0.2)', width=6,),
+#         ))
 for site in sites:
     fig2.add_trace(
         go.Scatter(x=df_plot_sites.index, y=df_plot_sites[site], name=site)
@@ -106,3 +119,4 @@ st.plotly_chart(fig2, use_container_width=True)
 
 # TODO
 # add plot for chosen postcodes
+# with select option - for displying "QLD mean", "Postcode MEAN"
